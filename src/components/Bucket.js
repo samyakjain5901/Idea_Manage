b2cc6693ff8eff7c61005f4c3c844e565c8d182a
Samyak Jain <samyak.jain5901@gmail.com>
2021-09-08 12:44:24 +0530

    Final

--- src/components/Bucket.js
+++ src/components/Bucket.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Note from "./Note";
 import { Draggable } from "react-beautiful-dnd";
-import CustomDrag from "react-drag-element";
-// import OutsideWrapper from "./OutsideWrapper";
 
-export default function Bucket({
-  showGroupedData,
-  id,
-  allNotes,
-  allBuckets,
-  deleteNoteFromBucket,
-  editNoteFromBucket,
-}) {
-  if (!showGroupedData) {
-    return (
-      <React.Fragment>
-        {/* <OutsideWrapper showGroupedData={this.props.showGroupedData}> */}
-        {allNotes &&
-          allNotes.map((eachNote) => {
-            return (
-              <CustomDrag dragItemId={eachNote.id} dragId={"draggable-area-id"}>
+class Bucket extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeDrags: 0,
+    };
+  }
+
+  render() {
+    const {
+      showGroupedData,
+      id,
+      addSelectedNotes,
+      allNotes,
+      allBuckets,
+      deleteNoteFromBucket,
+      editNoteFromBucket,
+    } = this.props;
+    if (!showGroupedData) {
+      return (
+        <React.Fragment>
+          {allNotes &&
+            allNotes.map((eachNote) => {
+              return (
                 <div style={{ width: "170px", display: "inline-block" }}>
                   <Draggable
                     key={eachNote.id}
@@ -28,7 +34,6 @@ export default function Bucket({
                   >
                     {(provided) => (
                       <div
-                        id="draggable-area-id"
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                         ref={provided.innerRef}
@@ -36,6 +41,7 @@ export default function Bucket({
                         <Note
                           showGroupedData={showGroupedData}
                           bucketId={id}
+                          addSelectedNotes={addSelectedNotes}
                           id={eachNote.id}
                           mainContent={eachNote.body}
                           userName={eachNote.name}
@@ -43,70 +49,74 @@ export default function Bucket({
                           allBuckets={allBuckets}
                           deleteNodeFromList={deleteNoteFromBucket}
                           editNoteFromBucket={editNoteFromBucket}
+                          ctrlPressed={this.props.ctrlPressed}
+                          selectedNotes={this.props.selectedNotes}
                         />
                       </div>
                     )}
                   </Draggable>
                 </div>
-              </CustomDrag>
-            );
-          })}
-
-        {/* </OutsideWrapper> */}
-      </React.Fragment>
-    );
-  }
-  return (
-    <div
-      className="group"
-      style={{
-        display: showGroupedData ? "inline-block" : "contents",
-        width: !showGroupedData ? "2px" : "auto",
-      }}
-    >
-      <div className="text-center">
-        <span
-          className="head-box"
-          style={{ display: showGroupedData ? "inline-block" : "none" }}
+              );
+            })}
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <div
+          className="group"
+          style={{
+            display: showGroupedData ? "inline-block" : "contents",
+            width: !showGroupedData ? "2px" : "auto",
+          }}
         >
-          {id}
-        </span>
-      </div>
-      <div style={{ display: showGroupedData ? "block" : "inline" }}>
-        {allNotes &&
-          allNotes.map((eachNote) => {
-            return (
-              <div style={{ width: "175px", display: "inline-block" }}>
-                <Draggable
-                  key={eachNote.id}
-                  draggableId={eachNote.id}
-                  index={eachNote.id}
-                >
-                  {(provided) => (
-                    <div
+          <div className="text-center">
+            <span
+              className="head-box"
+              style={{ display: showGroupedData ? "inline-block" : "none" }}
+            >
+              {id}
+            </span>
+          </div>
+          <div style={{ display: showGroupedData ? "block" : "inline" }}>
+            {allNotes &&
+              allNotes.map((eachNote) => {
+                return (
+                  <div style={{ width: "175px", display: "inline-block" }}>
+                    <Draggable
                       key={eachNote.id}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      ref={provided.innerRef}
+                      draggableId={eachNote.id}
+                      index={eachNote.id}
                     >
-                      <Note
-                        showGroupedData={showGroupedData}
-                        bucketId={id}
-                        id={eachNote.id}
-                        mainContent={eachNote.body}
-                        userName={eachNote.name}
-                        color={eachNote.color}
-                        allBuckets={allBuckets}
-                        deleteNodeFromList={deleteNoteFromBucket}
-                        editNoteFromBucket={editNoteFromBucket}
-                      />
-                    </div>
-                  )}
-                </Draggable>
-              </div>
-            );
-          })}
-      </div>
-    </div>
-  );
+                      {(provided) => (
+                        <div
+                          key={eachNote.id}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                        >
+                          <Note
+                            showGroupedData={showGroupedData}
+                            bucketId={id}
+                            id={eachNote.id}
+                            addSelectedNotes={addSelectedNotes}
+                            mainContent={eachNote.body}
+                            userName={eachNote.name}
+                            color={eachNote.color}
+                            allBuckets={allBuckets}
+                            deleteNodeFromList={deleteNoteFromBucket}
+                            editNoteFromBucket={editNoteFromBucket}
+                          />
+                        </div>
+                      )}
+                    </Draggable>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      );
+    }
+  }
 }
+
+export default Bucket;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,23 +1,56 @@
 import React from "react";
 import { Button } from "reactstrap";
 
-export default function Note({ id, showGroupedData, editNoteFromBucket, deleteNodeFromList, bucketId, mainContent = "mainContent", userName = "userName", color }) {
+export default function Note({
+  id,
+  showGroupedData,
+  addSelectedNotes,
+  editNoteFromBucket,
+  deleteNodeFromList,
+  bucketId,
+  mainContent = "mainContent",
+  userName = "userName",
+  color,
+  ctrlPressed,
+  selectedNotes,
+}) {
   function deleteNote() {
     deleteNodeFromList(bucketId, id);
   }
   function editNode() {
     editNoteFromBucket(bucketId, id, userName, mainContent, color);
   }
+
+  const currentNoteSelected =
+    ctrlPressed &&
+    selectedNotes.find((eachSelectedNote) => eachSelectedNote.noteId === id);
+
   return (
-    <div className="highlight" style={{backgroundColor: color, paddingLeft: '15px'}}>
-      {!showGroupedData ? <span className="note-head">{bucketId}</span>: <span></span>}
+    <div
+      onClick={() => {
+        if (ctrlPressed) {
+          addSelectedNotes(bucketId, id);
+        }
+      }}
+      className="highlight"
+      style={{
+        backgroundColor: color,
+        paddingLeft: "15px",
+        border: currentNoteSelected ? "2px solid blue" : "none",
+      }}
+    >
+      {!showGroupedData ? (
+        <span className="note-head">{bucketId}</span>
+      ) : (
+        <span></span>
+      )}
       <p>{mainContent}</p>
       <p>- {userName}</p>
       <Button color="link" onClick={deleteNote}>
         <span className="fa fa-trash fa-sm"></span>
       </Button>
       <Button color="link" onClick={editNode}>
-          <span className="fa fa-pencil fa-sm"></span>
+        <span className="fa fa-pencil fa-sm"></span>
       </Button>
     </div>
   );
